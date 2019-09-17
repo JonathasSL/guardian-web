a620e0bdf766acf8e4e10f0e3b176804e52e160f
Bruno Marini <bruno.marini1@gmail.com>
2019-09-17 16:32:16 -0300

    Bootstrap-Vue [+]

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 // --- IMPORTS ---
-// Essentials
+// ~ Third party ~
+// Bootstrap
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// ~ Essentials ~
 import Vue from 'vue';
 import router from './router';
 import store from './store';
@@ -7,6 +13,10 @@ import store from './store';
 // Root component
 import App from './App.vue';
 
+// --- USES ---
+Vue.use(BootstrapVue);
+
+// --- CONFIGS ---
 // Prevent the production tip on Vue startup
 Vue.config.productionTip = false;
 
