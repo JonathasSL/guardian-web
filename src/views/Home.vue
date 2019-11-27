506ece613fc62b792bf7c399cb62f866954a535e
Bruno Marini <bruno.marini1@gmail.com>
2019-11-27 10:22:34 -0200

    Unnused code [-]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,26 +61,7 @@
 				</div>
 			</nav>
       <main class="h-100 w-100 bg-dark">
-				<div class="right d-flex justify-content-center align-items-center m-2">
-					<div v-if="user" class="btn-group _rounded shadow-sm" role="group">
-					  <button v-if="user.type == 'driver'" title="Lista de Veículos" @click="showVehicleList = true" type="button" class="cars btn _rounded-tl _rounded-bl ">
-							<img src="@/assets/icon/car.svg" alt="">
-						</button>
-					  <button v-else title="Lista de Vagas" @click="showSpotList = true" type="button" class="btn btn-light _rounded-tl _rounded-bl ">
-							<img src="@/assets/icon/rectangles.svg" alt="">
-						</button>
-						<button title="Perfil" class="user btn px-4 shadow-sm _rounded-tr _rounded-br">
-							{{user.name}}
-						</button>
-						<button title="logout" @click="user = null" type="button" class="logout btn btn-dark _rounded ml-2">
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-								<path fill="none" d="M0 0h24v24H0V0z"/>
-								<path fill="white" d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
-							</svg>
-						</button>
-					</div>
-					<button @click="showLogin = true" v-if="!user" class="login btn btn-dark px-4 ml-2 _rounded">Login</button>
-				</div>
+				
       </main>
     </div>
     <Login @close="showLogin = false" @logged="logged" v-if="showLogin" class="login-container h-100 w-100"></Login>
