6109d7a2d8ee997d63c188d7dbf8a598fe11f4bd
Bruno Marini <bruno.marini1@gmail.com>
2019-12-01 14:24:40 -0200

    Maps [+] | Markers [+]

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+// GMaps
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 // ~ Essentials ~
 import Vue from 'vue';
 import router from './router';
@@ -15,6 +18,14 @@ import App from './App.vue';
 
 // --- USES ---
 Vue.use(BootstrapVue);
+Vue.use(VueGoogleMaps, {
+	load: {
+		key: '',
+		libraries: 'places',
+		region: 'BR',
+		language: 'pt-BR',
+	}
+});
 
 // --- CONFIGS ---
 // Prevent the production tip on Vue startup
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,7 +61,29 @@
 				</div>
 			</nav>
       <main class="h-100 w-100 bg-dark">
-				
+				<GmapMap
+					ref="mapRef"
+				  :center="{lat: -19.9296346, lng:-43.9375731}"
+				  :zoom="15"
+				  map-type-id="roadmap"
+				  style="width: 100%; height: 100%"
+				>
+				  <GmapMarker
+				    :key="index"
+				    v-for="(m, index) in markers"
+				    :position="m.position"
+				    :clickable="true"
+				    :draggable="true"
+				    @click="center=m.position"
+				  />
+				</GmapMap>
+				<!-- <GmapInfoWindow
+					:key="index"
+					v-for="(i, index) in infos"
+					:position="i.position"
+					:content="i.content"
+				/> -->
+			</GmapMap>
       </main>
     </div>
     <Login @close="showLogin = false" @logged="logged" v-if="showLogin" class="login-container h-100 w-100"></Login>
@@ -86,6 +108,11 @@ import SpotList from '@/components/SpotList.vue';
 import Garage from '@/components/Garage.vue';
 import PriceTable from '@/components/PriceTable.vue';
 
+// Axios
+import axios from 'axios';
+// BUGFIX: same Vue CLI Service URL for CORS with Cue CLI proxy (look at "vue.config.js" file)
+axios.defaults.baseURL = 'http://localhost:4242';
+
 export default {
   name: 'home',
   components: {
@@ -98,6 +125,12 @@ export default {
 		Garage,
 		PriceTable,
   },
+	created () {
+		this.getMap().then(map => {
+			this.markers = map.markers;
+			this.infos = map.infos;
+		});
+	},
   data() {
     return {
       showLogin: false,
@@ -107,6 +140,15 @@ export default {
 			showPriceTable: false,
 			showCars: false,
 			user: null,
+			infos: [],
+			markers: [],
+			// markers: [{
+			// 	position: {
+			// 		lat: -19.9296346,
+			// 		lng: -43.9375731,
+			// 	}
+			// }]
+			// center: null,
     }
   },
 	methods: {
@@ -117,7 +159,40 @@ export default {
 
 		create() {
 
-		}
+		},
+
+		getMap () {
+			return axios.get('/api/parking')
+				.then(response => {
+					let parkings = response.data;
+					let markers = []
+					let infos = []
+					for (let parking of parkings) {
+						if (parking.latitude && parking.longitude) {
+							markers.push({
+								position: {
+									lat: parking.latitude,
+									lng: parking.longitude,
+								}
+							});
+							infos.push({
+								position: {
+									lat: parking.latitude,
+									lng: parking.longitude,
+								},
+								content: "OLA MUNDO"
+							});
+						}
+					}
+
+					return {
+						markers: markers,
+						infos: infos,
+					};
+				}).catch(e => {
+					console.log(e);
+				})
+		},
 	}
 };
 </script>
